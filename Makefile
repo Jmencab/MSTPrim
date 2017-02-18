73c27c534de82ea3b6d69bedc50e565461d4c0c6
Julio <mendezcabrera@college.harvard.edu>
2017-02-18 13:06:15 -0500

    Makefile added heap; still working on list builder

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
-randmst : randmst.o randmst_tests.o 
+randmst : randmst.o randmst_tests.o heap.o
 
 randmst.o : randmst.c randmst.h
 	cc -c -ggdb randmst.c
 randmst_tests.o : randmst_tests.c randmst.h 
 	cc -c -ggdb randmst_tests.c
+heap.0: heap.c randmst.h
+	cc -c -ggdb heap.c
 
 clean :
-	rm randmst randmst.o randmst_tests.o 
+	rm randmst randmst.o randmst_tests.o heap.o
--- randmst.c
+++ randmst.c
@@ -1,6 +1,5 @@
 #include "randmst.h"
-#define RANDMOD 10000
-
+#define PROB 0.0110485435
 /* Want to be able to run ./randmst 0 numpoints numtrials dimension
 We ultimately want an adjacency list that gives the distance to another 
 node from one node along relevant edges. */
@@ -56,6 +55,7 @@ lpoint* new_lpoint(void){
   //initialize distance
   l_pointer-> dist = 0;
   l_pointer->next_lpoint = NULL;
+  l_point->point = -1;
   return l_pointer;
 }
 
@@ -78,13 +78,7 @@ void build_graph(int numpoints, int dimensions, node* nodes[], time_t t){
   array_initializer(nodes, numpoints);
 
    switch(dimensions){
-     case 0:
-        for(int i = 0; i < numpoints; i++){
 
-          nodes[i]->coord[0] = rand_range(t, 1);
-        }
-        break;
-  	
      case 2:
         for(int i = 0; i < numpoints; i++){
           nodes[i]->coord[0] = rand_range(t, 1);
@@ -112,6 +106,160 @@ void build_graph(int numpoints, int dimensions, node* nodes[], time_t t){
 }
 
 /*TODO TWO: Build adjacency lists by pruning distances greater than radius*/
+void list_builder(int numpoints, int dimensions, node* nodes[], time_t t){
+
+  switch(dimensions){
+    case 0:
+      //need to generate edge weights here individually
+      for(int i = 0; i < numpoints; i++){
+        lpoint* head = NULL;
+
+          for(int j = i; j < numpoints; j++){
+
+            if(nodes[i]->first_l){
+              float wedge = rand_range(t, 1);
+              if(wedge <= PROB){
+                lpoint* next_edge = new_lpoint();
+                assert(next_edge);
+                next_edge->dist = wedge;
+                next_edge->point = j;
+                head->next_lpoint = next_edge;
+                head = next_edge;
+              }
+            }
+
+            if(!nodes[i]->first_l){
+              float wedge = rand_range(t, 1);
+              if(wedge <= PROB){
+                lpoint* first_edge = new_lpoint();
+                assert(first_edge);
+                first_edge->dist = wedge;
+                first_edge->point = j;
+                nodes[i]->first_l = first_edge;
+                head = first_edge;
+              }
+            }
+          }
+      }
+    break;
+
+    //for all other cases, need to calculate edge weights based on coordinates
+    case 2:
+      for(int i = 0; i < numpoints; i++){
+        lpoint* head = NULL;
+        float x_1 = nodes[i]-> coord[0];
+        float y_1 = nodes[i]-> coord[1];
+        for(int j = i + 1; j < numpoints; j++){
+          float x_2 = nodes[j]->coord[0];
+          float y_2 = nodes[j]->coord[1];
+          double x_diff = (double) (x_2 - x_1);
+          double y_diff = (double) (y_2 -y_1);
+          double x_diff_square = pow(x_diff, 2);
+          double y_diff_square = pow(y_diff, 2);
+          float distance = (float) sqrt((x_diff_square + y_diff_square));
+          if(distance <= PROB){
+            if(nodes[i]->first_l){
+              lpoint* next_edge = new_lpoint();
+              assert(next_edge);
+              next_edge-> dist = distance;
+              head->next_lpoint = next_edge;
+              head = next_edge;
+            }
+            if(!nodes[i]->first_l){
+              lpoint* first_edge = new_lpoint();
+              assert(first_edge);
+              first_edge-> dist = distance;
+              nodes[i]-> first_l = first_edge;
+              head = first_edge;
+            }
+          }
+        }
+      }
+    break;
+
+    case 3: 
+      for(int i = 0; i < numpoints; i++){
+          lpoint* head = NULL;
+          float x_1 = nodes[i]-> coord[0];
+          float y_1 = nodes[i]-> coord[1];
+          float z_1 = nodes[i]-> coord[2];
+          for(int j = i + 1; j < numpoints; j++){
+            float x_2 = nodes[j]-> coord[0];
+            float y_2 = nodes[j]-> coord[1];
+            float z_2 = nodes[j]-> coord[2];
+            double x_diff = (double) (x_2 - x_1);
+            double y_diff = (double) (y_2 - y_1);
+            double z_diff = (double) (z_2 - z_1);
+            double x_diff_square = pow(x_diff, 2);
+            double y_diff_square = pow(y_diff, 2);
+            double z_diff_square = pow(z_diff, 2);
+            float distance = 
+              (float) sqrt((x_diff_square + y_diff_square + z_diff_square));
+            if(distance <= PROB){
+              if(nodes[i]->first_l){
+                lpoint* next_edge = new_lpoint();
+                assert(next_edge);
+                next_edge-> dist = distance;
+                head->next_lpoint = next_edge;
+                head = next_edge;
+              }
+              if(!nodes[i]->first_l){
+                lpoint* first_edge = new_lpoint();
+                assert(first_edge);
+                first_edge-> dist = distance;
+                nodes[i]-> first_l = first_edge;
+                head = first_edge;
+              }
+            }
+          }
+        }
+      break;
+
+      case 4: 
+      for(int i = 0; i < numpoints; i++){
+          lpoint* head = NULL;
+          float x_1 = nodes[i]-> coord[0];
+          float y_1 = nodes[i]-> coord[1];
+          float z_1 = nodes[i]-> coord[2];
+          float a_1 = nodes[i]-> coord[3];
+          for(int j = i + 1; j < numpoints; j++){
+            float x_2 = nodes[j]-> coord[0];
+            float y_2 = nodes[j]-> coord[1];
+            float z_2 = nodes[j]-> coord[2];
+            float a_2 = nodes[j]-> coord[3];
+            double x_diff = (double) (x_2 - x_1);
+            double y_diff = (double) (y_2 - y_1);
+            double z_diff = (double) (z_2 - z_1);
+            double a_diff = (double) (a_2 - a_1);
+            double x_diff_square = pow(x_diff, 2);
+            double y_diff_square = pow(y_diff, 2);
+            double z_diff_square = pow(z_diff, 2);
+            double a_diff_square = pow(a_diff, 2);
+            float distance = 
+              (float) sqrt((x_diff_square + y_diff_square + 
+              z_diff_square + a_diff_square));
+            if(distance <= PROB){
+              if(nodes[i]->first_l){
+                lpoint* next_edge = new_lpoint();
+                assert(next_edge);
+                next_edge-> dist = distance;
+                head->next_lpoint = next_edge;
+                head = next_edge;
+              }
+              if(!nodes[i]->first_l){
+                lpoint* first_edge = new_lpoint();
+                assert(first_edge);
+                first_edge-> dist = distance;
+                nodes[i]-> first_l = first_edge;
+                head = first_edge;
+              }
+            }
+          }
+        }
+      break;
+    }
+}
+
 int main(int argc, char** argv){
   if(argc > 5 || argc < 1)
     EXIT_FAILURE;
