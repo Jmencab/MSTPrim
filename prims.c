c550df5f11c33888d89291572bc44cb8d3c6fe26
Phillip Huang <philliphuang@college.harvard.edu>
2017-02-27 15:07:22 -0500

    commented prims

--- prims.c
+++ prims.c
@@ -5,30 +5,53 @@
  * Implementation of Prim's Algorithm in C
  * HUID: 40940510
  *
- * USAGE SUMMARY: 
- *
  */
 
+/* INPUTS:
+ * G: the graph
+ * size: size of graph
+ * root: index of root of MST
+ * dist: distance array
+ * prev: prev array
+ * 
+ * OUTPUTS:
+ *  0 : all good
+ * -1 : error
+ *
+ */
 void prim(node* G[], int size, int root, float dist[], int prev[]) {
-	int set[size];
-	heap* H = init_heap(size);
-	min_heap_insert(H, root, 0);
+	/* Initialize */
+	int set[size]; // set S of vertices in MST
+	heap* H = init_heap(size); // init heap
+	min_heap_insert(H, root, 0); // place root in heap
+
+	/* For each vertex, initialize */
 	for (int i = 0; i < size; i++) {
 		dist[i] = 999; // represents infinity
 		prev[i] = -1; // represents nil
 		set[i] = 0; // represents not in S
 	}
 	dist[root] = 0;
+
+	// Extract root
 	int v = heap_extract_min(H);
-	while (v != -1){
-		set[v] = 1;	
-			if(G[v]->first_l){
-			lpoint* head = G[v]->first_l;
-				while(head){
+	while (v != -1){ // while heap remains
+		set[v] = 1;	// add new vertex to set S
+			/* if any edge exists from this vertex */
+			if (G[v]->first_l) { 
+
+				lpoint* head = G[v]->first_l;
+
+				/* for each edge from vertex */
+				while (head) {
 					int w = head->vertex;
-					if(set[w] == 0){
-						if(dist[w] > head -> dist){
-							dist[w] = head -> dist;
+
+					/* if edge leads to new vertex */
+					if (set[w] == 0) {
+
+						/* insert into heap if new distance smaller */
+						if (dist[w] > head->dist) {
+							dist[w] = head->dist;
 							prev[w] = v; 
 							min_heap_insert(H, w, dist[w]);
 						}
@@ -36,6 +59,9 @@ void prim(node* G[], int size, int root, float dist[], int prev[]) {
 					head = head->next_lpoint;
 				}
 			}
+			// else {
+				// TODO: IMPLEMENT ERROR HERE
+			// }
 		v = heap_extract_min(H);
 	}
 	destroy_heap(H);
